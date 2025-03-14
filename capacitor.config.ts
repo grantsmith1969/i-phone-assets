c8470bdb145a2784253777e320fbc31394289f10
grantsmith1969 <work.grant@gmail.com>
2025-03-14 00:31:24 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iPhone Assets',
+  webDir: 'dist'
+};
+
+export default config;
